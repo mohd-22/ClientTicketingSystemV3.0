3bc714faf9c291cc37e3a4a96291bf432a093d54
MOHAMMED RA'ED DAWAH <mrdawah22@cit.just.edu.jo>
2026-05-21 16:17:32 +0300

    Backend : Add Assign Service

---
+++ ClientTicketingSystem_back/ClientTicketingSystem/ClientTicketingSystem.Application/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,7 @@
+﻿using ClientTicketingSystem.Application.Helpers;
+
+namespace ClientTicketingSystem.Application.Services.Interfaces;
+public interface IEmployeeService
+{
+    Task<ApiResponse<bool>> AssignTicketToEmployee(Guid TicketId, Guid EmployeeId);
+}
---
+++ ClientTicketingSystem_back/ClientTicketingSystem/ClientTicketingSystem/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using ClientTicketingSystem.Application.Services;
+using ClientTicketingSystem.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientTicketingSystem.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class EmployeesController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeesController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    [HttpPut("assign/{ticketId}")]
+    public async Task<IActionResult> AssignTicket(Guid ticketId, [FromQuery] Guid employeeId)
+    {
+        var result = await _employeeService.AssignTicketToEmployee(ticketId, employeeId);
+        return StatusCode(result.StatusCode, result);
+    }
+}
